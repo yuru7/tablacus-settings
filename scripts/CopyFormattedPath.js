190d95c833c17bca64e32e4a5316f92dd70f1270
tawara <13458509+yuru7@users.noreply.github.com>
2020-07-29 16:43:05 +0900

    Update CopyFormattedPath.js

--- scripts/CopyFormattedPath.js
+++ scripts/CopyFormattedPath.js
@@ -1,37 +1,39 @@
 // 何も選択していない場合にはフォルダパスをコピーして終了
 if (FV.SelectedItems() == 0) {
-	clipboardData.setData("text", FV.FolderItem.Path);
-	return
+  clipboardData.setData('text', FV.FolderItem.Path);
+  return;
 }
 
 var clipText = buildClipText(FV);
-clipboardData.setData("text", clipText);
+clipboardData.setData('text', clipText);
 
 function buildClipText(FV) {
-	var items = FV.SelectedItems();
-	
-	// ファイル・フォルダパスで match をかけると各インデックスに以下の値が格納される
-	// 0: フルパス / 1: ドライブ / 2: 親フォルダ / 3: ファイル・フォルダ名
-	var pattern = /(?:(\w+):|\\)(?:\\([^\\:\*?\"<>\|]+))*(?:\\(([^:\\\*?\"<>\|]+)$))/;
-	
-	var clipText = "";
-	var beforeParentFolder = "";
-	
-	for (var i = items.Count; i-- > 0;) {
-		var result = items[i].Path.match(pattern);
-		
-		var parentPath = fso.GetParentFolderName(items[i].Path);
-		if (beforeParentFolder != parentPath) {
-		if (i < items.Count - 1) {
-			// 最初のアイテムではない場合は、1行空ける
-			clipText = clipText + "\n";
-		}
-			clipText = clipText + parentPath + "\n";
-		}
-		beforeParentFolder = parentPath;
-	
-		clipText = clipText + "└ " + result[3] + "\n";
-	}
-	
-	return clipText;
+  var items = FV.SelectedItems();
+
+  // ファイル・フォルダパスで match をかけると各インデックスに以下の値が格納される
+  // 0: フルパス / 1: ドライブ / 2: 親フォルダ / 3: ファイル・フォルダ名
+  var pattern = /(?:(\w+):|\\)(?:\\([^\\:\*?\"<>\|]+))*(?:\\(([^:\\\*?\"<>\|]+)$))/;
+
+  var clipText = '';
+  var beforeParentFolder = '';
+
+  var pathMap = new Map();
+  for (var i = items.Count; i-- > 0; ) {
+    var result = items[i].Path.match(pattern);
+    var parentPath = fso.GetParentFolderName(items[i].Path);
+
+    // 親フォルダをキーにしたファイル一覧マップを生成
+    if (pathMap.get(parentPath) == void 0) {
+      pathMap.set(parentPath, '└ ' + result[3]);
+    } else {
+      pathMap.set(parentPath, pathMap.get(parentPath) + '\n└ ' + result[3]);
+    }
+  }
+
+  clipText = '';
+  pathMap.forEach(function (value, key) {
+    clipText = clipText + key + '\n' + value + '\n\n';
+  });
+
+  return clipText;
 }
