bfac574be4223d92e5aa7c81df93b1cbf85d59a5
tawara <13458509+yuru7@users.noreply.github.com>
2021-05-02 18:14:24 +0900

    Update AutoColumnsSize.js

--- scripts/AutoColumnsSize.js
+++ scripts/AutoColumnsSize.js
@@ -2,8 +2,8 @@ FV.CurrentViewMode(4,16);
 setTimeout(function()
 {
   // カラム幅の自動調整
-  var cols = FV.Columns;
-  cols = cols.replace(/"名前" [0-9]+/, '"System.ItemNameDisplay" -2'); // ファイル名
+  var cols = FV.GetColumns(1);
+  cols = cols.replace(/"System.ItemNameDisplay" [0-9]+/, '"System.ItemNameDisplay" -2'); // ファイル名
   
   FV.Columns = cols;
 }, 99);
