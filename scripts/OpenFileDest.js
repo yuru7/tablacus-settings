e99ae89696d586dcd9f1f3ab059073035f4a6046
tawara <13458509+yuru7@users.noreply.github.com>
2022-02-02 21:08:00 +0900

    Update and rename OpenLnkFileDest.js to OpenFileDest.js

--- scripts/OpenFileDest.js
+++ scripts/OpenFileDest.js
@@ -5,15 +5,17 @@ function main() {
     return;
   }
   var focusedPath = FV.FocusedItem.Path;
+  var targetPath = null;
   if (/\.lnk$/.test(focusedPath)) {
     var sc = wsh.CreateShortcut(focusedPath);
     var scPath = sc.TargetPath;
-    var targetPath = null;
     if (fso.FileExists(scPath) || fso.FolderExists(scPath)) {
       targetPath = fso.GetParentFolderName(scPath);
     }
-    if (targetPath) {
-      Navigate(targetPath, SBSP_NEWBROWSER);
-    }
+  } else if (fso.FileExists(focusedPath) || fso.FolderExists(focusedPath)) {
+    targetPath = fso.GetParentFolderName(focusedPath);
+  }
+  if (targetPath) {
+    Navigate(targetPath, SBSP_NEWBROWSER);
   }
 }
